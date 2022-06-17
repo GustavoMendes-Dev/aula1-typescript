b1ceda0f147c630a8a473a1e3b92e3d8def8f915
Gustavo Mendes <g.mendes93@hotmail.com>
2022-06-17 10:36:25 -0300

    feature: Commit Initial

---
+++ src/routes.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+interface IRoutes {
+  route: string;
+}
+
+function Routes() {
+const [ routes, setRoutes ] = useState<IRoutes[]>([]);
+
+  return (
+    <div></div>
+  );
+}
+
+export default Routes;
